1ab8b4cde585b6ca345b0a315bbf78e7f0a5d50b
ktpunnisa <ktpunnisa@gmail.com>
2019-11-23 14:53:57 +0700

    edit dockerfile

--- service-user/src/config.js
+++ service-user/src/config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const PORT = process.env.PORT || 5000;
+export const PORT = process.env.PORT || 7000;
 export const db = {
   host: process.env.MYSQL_HOST || "localhost",
   user: process.env.MYSQL_USER || "root",
