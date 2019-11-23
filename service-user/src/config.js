2e729d288f91af0dede2cbe36576e42e68b66ec6
ktpunnisa <ktpunnisa@gmail.com>
2019-11-23 13:07:23 +0700

    delete dedt_service

--- service-user/src/config.js
+++ service-user/src/config.js
@@ -6,7 +6,7 @@ export const PORT = process.env.PORT || 5000;
 export const db = {
   host: process.env.MYSQL_HOST || "localhost",
   user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PASSWORD || "",
+  password: process.env.MYSQL_PASSWORD || "root",
   database: process.env.MYSQL_DATABASE || "gmm_user",
   port: process.env.MYSQL_PORT || "3306"
 };
