70b9983ba6e1c54c49703970716cafb4ee839650
Natchaporn Ponkitsaran <natchaporn.pon@gmail.com>
2019-11-23 11:49:40 +0000

    Update build-docker-image

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -3,9 +3,13 @@
 docker build -t npmoewii/gmm-database ./database/
 docker build -t npmoewii/gmm-frontend ./frontend/
 docker build -t npmoewii/gmm-service-user ./service-user/
+docker build -t npmoewii/gmm-service-debt ./service-debt/
+docker build -t npmoewii/gmm-service-transaction ./service-transaction/
 
 docker login
 docker push npmoewii/gmm-database
 docker push npmoewii/gmm-frontend
 docker push npmoewii/gmm-service-user
+docker push npmoewii/gmm-service-debt
+docker push npmoewii/gmm-service-transaction
 docker logout
