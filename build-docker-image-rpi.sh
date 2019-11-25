9ff34568f1ffd46a0297e94e034cb3eefef00105
Natchaporn Ponkitsaran <natchaporn.pon@gmail.com>
2019-11-25 11:24:38 +0000

    Fix build image for rpi

--- build-docker-image-rpi.sh
+++ build-docker-image-rpi.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-docker build -f rpi.dockerfile -t npmoewii/gmm-database:rpi ./database/
-docker build -f rpi.dockerfile -t npmoewii/gmm-frontend:rpi ./frontend/
-docker build -f rpi.dockerfile -t npmoewii/gmm-service-user:rpi ./service-user/
-docker build -f rpi.dockerfile -t npmoewii/gmm-service-debt:rpi ./service-debt/
-docker build -f rpi.dockerfile -t npmoewii/gmm-service-transaction:rpi ./service-transaction/
+docker build -f ./database/rpi.dockerfile -t npmoewii/gmm-database:rpi ./database/
+docker build -f ./frontend/rpi.dockerfile -t npmoewii/gmm-frontend:rpi ./frontend/
+docker build -f ./service-user/rpi.dockerfile -t npmoewii/gmm-service-user:rpi ./service-user/
+docker build -f ./service-debt/rpi.dockerfile -t npmoewii/gmm-service-debt:rpi ./service-debt/
+docker build -f ./service-transaction/rpi.dockerfile -t npmoewii/gmm-service-transaction:rpi ./service-transaction/
 
 docker login
 docker push npmoewii/gmm-database:rpi
